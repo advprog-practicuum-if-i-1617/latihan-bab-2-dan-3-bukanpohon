7b3d581a1098d04dde7a903ddbfdfe4eed35c747
bukanpohon <jatixnugrohoo@gmail.com>
2017-03-24 00:44:12 +0700

    fix InsyaAllah

---
+++ src/latihan/mainkonversi.java
@@ -0,0 +1,12 @@
+package latihanbab2dan3;
+
+import java.util.Scanner;
+
+public class mainkonversi {
+
+    public static void main(String[] args) {
+        konversi konversi1 = new konversi();
+        System.out.println(konversi1.konversi("lima ratus enam puluh sembilan"));
+        System.out.println(konversi1.Konversi(480));
+    }
+}
